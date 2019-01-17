ff72a9dbd9bec469c149d83367d417b718c52bf5
Raine Virta <raine.virta@gmail.com>
2019-01-17 12:24:03 +0200

    add endOnUnhook option

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const { PassThrough } = require('stream')
 
-module.exports = (hookedStream) => {
+const DEFAULT_OPTIONS = {
+  endOnUnhook: true
+}
+
+module.exports = (hookedStream, opts = {}) => {
+  opts = { DEFAULT_OPTIONS, ...opts }
   const originalWrite = hookedStream.write.bind(hookedStream)
   const newStream = new PassThrough()
 
   const unhook = () => {
     hookedStream.write = originalWrite
-    newStream.end()
+    if (opts.endOnUnhook) newStream.end()
   }
 
   hookedStream.write = (...args) => {
@@ -14,5 +19,5 @@ module.exports = (hookedStream) => {
     return originalWrite(...args)
   }
 
-  return [ unhook, newStream ]
+  return [unhook, newStream]
 }
